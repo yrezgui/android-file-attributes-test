12fbe140b4e0bdd36c5ef6ed8e14f9638fbc0928
Yacine Rezgui <yrezgui@google.com>
2021-12-02 23:16:03 +0000

    Move storage functions to a dedicated file

--- app/src/main/java/com/samples/fileattributestest/MainActivity.kt
+++ app/src/main/java/com/samples/fileattributestest/MainActivity.kt
@@ -1,22 +1,15 @@
 package com.samples.fileattributestest
 
 import android.Manifest.permission.READ_EXTERNAL_STORAGE
-import android.content.ContentUris
-import android.content.pm.PackageManager
-import android.media.MediaScannerConnection
-import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
-import android.provider.MediaStore.Files.FileColumns.DATE_ADDED
 import android.text.format.Formatter.formatShortFileSize
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.result.contract.ActivityResultContracts.StartIntentSenderForResult
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
@@ -28,6 +21,7 @@ import androidx.compose.material.ListItem
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -35,12 +29,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat
 import com.samples.fileattributestest.ui.theme.FileAttributesTestTheme
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.suspendCancellableCoroutine
-import java.io.File
-import kotlin.coroutines.resume
 
 class MainActivity : ComponentActivity() {
     @ExperimentalMaterialApi
@@ -49,7 +38,13 @@ class MainActivity : ComponentActivity() {
         setContent {
             FileAttributesTestTheme {
                 val context = LocalContext.current
-                var hasStoragePermission by remember { mutableStateOf(hasStoragePermission()) }
+                var hasStoragePermission by remember {
+                    mutableStateOf(
+                        StorageUtils.hasStoragePermission(
+                            context
+                        )
+                    )
+                }
 
                 val requestPermission =
                     rememberLauncherForActivityResult(ActivityResultContracts.RequestPermission()) {
@@ -61,7 +56,12 @@ class MainActivity : ComponentActivity() {
                 val intentSenderLauncher =
                     rememberLauncherForActivityResult(StartIntentSenderForResult()) { result ->
                         if (result.resultCode == RESULT_OK) {
-                            mostRecentImage?.let { editLastModificationDate(it.path) }
+                            mostRecentImage?.let {
+                                StorageUtils.editLastModificationDate(
+                                    context,
+                                    it.path
+                                )
+                            }
                         }
                     }
 
@@ -77,7 +77,9 @@ class MainActivity : ComponentActivity() {
                         ListItem {
                             Button(
                                 enabled = hasStoragePermission,
-                                onClick = { mostRecentImage = getMostRecentImage() }
+                                onClick = {
+                                    mostRecentImage = StorageUtils.getMostRecentImage(context)
+                                }
                             ) {
                                 Text("Get most recent image")
                             }
@@ -96,7 +98,8 @@ class MainActivity : ComponentActivity() {
                                     enabled = mostRecentImage != null,
                                     onClick = {
                                         intentSenderLauncher.launch(
-                                            editImageWithWriteRequest(
+                                            StorageUtils.editImageWithWriteRequest(
+                                                context,
                                                 mostRecentImage!!.uri
                                             )
                                         )
@@ -108,7 +111,10 @@ class MainActivity : ComponentActivity() {
                                 Button(
                                     enabled = mostRecentImage != null,
                                     onClick = {
-                                        editLastModificationDate(mostRecentImage!!.path)
+                                        StorageUtils.editLastModificationDate(
+                                            context,
+                                            mostRecentImage!!.path
+                                        )
                                     }
                                 ) {
                                     Text("Click to edit last item (without write request)")
@@ -121,161 +127,46 @@ class MainActivity : ComponentActivity() {
                         /**
                          * Display image metadata once it has been fetched
                          */
-                        mostRecentImage?.let { image ->
-                            ListItem(trailing = { Text("Filename") }) {
-                                Text(image.filename)
-                            }
-                            Divider()
-
-                            ListItem(trailing = { Text("Uri") }) {
-                                Text(image.uri.toString())
-                            }
-                            Divider()
-
-                            ListItem(trailing = { Text("Size") }) {
-                                Text(formatShortFileSize(context, image.size))
-                            }
-                            Divider()
-
-                            ListItem(trailing = { Text("Mime Type") }) {
-                                Text(image.mimeType)
-                            }
-                            Divider()
-
-                            ListItem(trailing = { Text("Last Modified") }) {
-                                Text(image.lastModified.toString())
-                            }
-                            Divider()
-
-                            ListItem(trailing = { Text("Path") }) {
-                                Text(image.path)
-                            }
-                            Divider()
-                        }
+                        mostRecentImage?.let { ImageDetails(it) }
                     }
                 }
             }
         }
     }
+}
 
-    /**
-     * Check if app has [READ_EXTERNAL_STORAGE] permission
-     */
-    private fun hasStoragePermission(): Boolean {
-        return ContextCompat.checkSelfPermission(
-            this,
-            READ_EXTERNAL_STORAGE
-        ) == PackageManager.PERMISSION_GRANTED
+@ExperimentalMaterialApi
+@Composable
+fun ImageDetails(image: ImageResource) {
+    val context = LocalContext.current
+
+    ListItem(trailing = { Text("Filename") }) {
+        Text(image.filename)
     }
+    Divider()
 
-    /**
-     * Logic to create a [MediaStore.createWriteRequest] for
-     * [ActivityResultContracts.StartIntentSenderForResult]
-     */
-    @RequiresApi(Build.VERSION_CODES.R)
-    private fun editImageWithWriteRequest(uri: Uri): IntentSenderRequest {
-        return IntentSenderRequest.Builder(
-            MediaStore.createWriteRequest(
-                contentResolver,
-                listOf(uri)
-            )
-        )
-            .build()
+    ListItem(trailing = { Text("Uri") }) {
+        Text(image.uri.toString())
     }
+    Divider()
 
-    /**
-     * Update the file last modification date with given path and re-scan it to update its metadata
-     * on MediaStore
-     */
-    private fun editLastModificationDate(path: String) {
-        val file = File(path)
-        val now = System.currentTimeMillis()
-        file.setLastModified(now)
-
-        runBlocking {
-            scanPath(file.absolutePath, "image/jpeg")
-            println("setLastModified: now($now) || lastModified(${file.lastModified()})")
-        }
+    ListItem(trailing = { Text("Size") }) {
+        Text(formatShortFileSize(context, image.size))
     }
+    Divider()
 
-    /**
-     * Scan a path to update its metadata on MediaStore
-     */
-    private suspend fun scanPath(path: String, mimeType: String): Uri? {
-        return suspendCancellableCoroutine { continuation ->
-            MediaScannerConnection.scanFile(
-                this,
-                arrayOf(path),
-                arrayOf(mimeType)
-            ) { _, scannedUri ->
-                if (scannedUri == null) {
-                    continuation.cancel(Exception("File $path could not be scanned"))
-                } else {
-                    continuation.resume(scannedUri)
-                }
-            }
-        }
+    ListItem(trailing = { Text("Mime Type") }) {
+        Text(image.mimeType)
     }
+    Divider()
 
-    data class ImageResource(
-        val uri: Uri,
-        val filename: String,
-        val size: Long,
-        val mimeType: String,
-        val lastModified: Long,
-        val path: String,
-    )
-
-    /**
-     * Query MediaStore to get the most recently added image
-     */
-    private fun getMostRecentImage(): ImageResource {
-        val imageCollection = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
-        } else {
-            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-        }
-
-        val projection = arrayOf(
-            MediaStore.Images.ImageColumns._ID,
-            MediaStore.Images.ImageColumns.DISPLAY_NAME,
-            MediaStore.Images.ImageColumns.SIZE,
-            MediaStore.Images.ImageColumns.MIME_TYPE,
-            MediaStore.Images.ImageColumns.DATE_MODIFIED,
-            MediaStore.Images.ImageColumns.DATA,
-        )
-
-        val cursor = contentResolver.query(
-            imageCollection,
-            projection,
-            null,
-            null,
-            "$DATE_ADDED DESC"
-        ) ?: throw Exception("Query could not be executed")
-
-        cursor.use {
-            if (!cursor.moveToFirst()) {
-                throw Exception("Could not find any images")
-            }
-
-            val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns._ID)
-            val displayNameColumn =
-                cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.DISPLAY_NAME)
-            val sizeColumn = cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.SIZE)
-            val mimeTypeColumn =
-                cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.MIME_TYPE)
-            val dateModifiedColumn =
-                cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.DATE_MODIFIED)
-            val dataColumn = cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.DATA)
-
-            return ImageResource(
-                uri = ContentUris.withAppendedId(imageCollection, cursor.getLong(idColumn)),
-                filename = cursor.getString(displayNameColumn),
-                size = cursor.getLong(sizeColumn),
-                mimeType = cursor.getString(mimeTypeColumn),
-                lastModified = cursor.getLong(dateModifiedColumn),
-                path = cursor.getString(dataColumn),
-            )
-        }
+    ListItem(trailing = { Text("Last Modified") }) {
+        Text(image.lastModified.toString())
     }
+    Divider()
+
+    ListItem(trailing = { Text("Path") }) {
+        Text(image.path)
+    }
+    Divider()
 }
---
+++ app/src/main/java/com/samples/fileattributestest/StorageUtils.kt
@@ -0,0 +1,141 @@
+package com.samples.fileattributestest
+
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.content.ContentUris
+import android.content.Context
+import android.content.pm.PackageManager
+import android.media.MediaScannerConnection
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.suspendCancellableCoroutine
+import java.io.File
+import kotlin.coroutines.resume
+
+data class ImageResource(
+    val uri: Uri,
+    val filename: String,
+    val size: Long,
+    val mimeType: String,
+    val lastModified: Long,
+    val path: String,
+)
+
+object StorageUtils {
+    /**
+     * Check if app has [READ_EXTERNAL_STORAGE] permission
+     */
+    fun hasStoragePermission(context: Context): Boolean {
+        return ContextCompat.checkSelfPermission(
+            context,
+            READ_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    /**
+     * Logic to create a [MediaStore.createWriteRequest] for
+     * [ActivityResultContracts.StartIntentSenderForResult]
+     */
+    @RequiresApi(Build.VERSION_CODES.R)
+    fun editImageWithWriteRequest(context: Context, uri: Uri): IntentSenderRequest {
+        return IntentSenderRequest.Builder(
+            MediaStore.createWriteRequest(
+                context.contentResolver,
+                listOf(uri)
+            )
+        )
+            .build()
+    }
+
+    /**
+     * Update the file last modification date with given path and re-scan it to update its metadata
+     * on MediaStore
+     */
+    fun editLastModificationDate(context: Context, path: String) {
+        val file = File(path)
+        val now = System.currentTimeMillis()
+        file.setLastModified(now)
+
+        runBlocking {
+            scanPath(context, file.absolutePath, "image/jpeg")
+            println("setLastModified: now($now) || lastModified(${file.lastModified()})")
+        }
+    }
+
+    /**
+     * Scan a path to update its metadata on MediaStore
+     */
+    private suspend fun scanPath(context: Context, path: String, mimeType: String): Uri? {
+        return suspendCancellableCoroutine { continuation ->
+            MediaScannerConnection.scanFile(
+                context,
+                arrayOf(path),
+                arrayOf(mimeType)
+            ) { _, scannedUri ->
+                if (scannedUri == null) {
+                    continuation.cancel(Exception("File $path could not be scanned"))
+                } else {
+                    continuation.resume(scannedUri)
+                }
+            }
+        }
+    }
+
+    /**
+     * Query MediaStore to get the most recently added image
+     */
+    fun getMostRecentImage(context: Context): ImageResource {
+        val imageCollection = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
+        } else {
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        }
+
+        val projection = arrayOf(
+            MediaStore.Images.ImageColumns._ID,
+            MediaStore.Images.ImageColumns.DISPLAY_NAME,
+            MediaStore.Images.ImageColumns.SIZE,
+            MediaStore.Images.ImageColumns.MIME_TYPE,
+            MediaStore.Images.ImageColumns.DATE_MODIFIED,
+            MediaStore.Images.ImageColumns.DATA,
+        )
+
+        val cursor = context.contentResolver.query(
+            imageCollection,
+            projection,
+            null,
+            null,
+            "${MediaStore.Files.FileColumns.DATE_ADDED} DESC"
+        ) ?: throw Exception("Query could not be executed")
+
+        cursor.use {
+            if (!cursor.moveToFirst()) {
+                throw Exception("Could not find any images")
+            }
+
+            val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns._ID)
+            val displayNameColumn =
+                cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.DISPLAY_NAME)
+            val sizeColumn = cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.SIZE)
+            val mimeTypeColumn =
+                cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.MIME_TYPE)
+            val dateModifiedColumn =
+                cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.DATE_MODIFIED)
+            val dataColumn = cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.DATA)
+
+            return ImageResource(
+                uri = ContentUris.withAppendedId(imageCollection, cursor.getLong(idColumn)),
+                filename = cursor.getString(displayNameColumn),
+                size = cursor.getLong(sizeColumn),
+                mimeType = cursor.getString(mimeTypeColumn),
+                lastModified = cursor.getLong(dateModifiedColumn),
+                path = cursor.getString(dataColumn),
+            )
+        }
+    }
+}
