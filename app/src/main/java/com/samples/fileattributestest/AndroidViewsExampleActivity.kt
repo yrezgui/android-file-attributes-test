01da1dffd45b0f4200cf2a832afe736c02231e5b
Yacine Rezgui <yrezgui@google.com>
2021-12-03 13:39:13 +0000

    Add Android Views example

---
+++ app/src/main/java/com/samples/fileattributestest/AndroidViewsExampleActivity.kt
@@ -0,0 +1,104 @@
+package com.samples.fileattributestest
+
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.samples.fileattributestest.databinding.AndroidViewsExampleActivityBinding
+
+class AndroidViewsExampleActivity : AppCompatActivity() {
+    private lateinit var binding: AndroidViewsExampleActivityBinding
+    private var mostRecentImage: ImageResource? = null
+
+    private val requestPermission =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+            updateUi()
+        }
+
+    private val intentSenderLauncher =
+        registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                mostRecentImage?.let {
+                    StorageUtils.editLastModificationDate(this, it.path)
+                    mostRecentImage = StorageUtils.getMostRecentImage(this)
+                    updateUi()
+                }
+            }
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.android_views_example_activity)
+
+        binding = AndroidViewsExampleActivityBinding.inflate(layoutInflater)
+        setupUi()
+        updateUi()
+        setContentView(binding.root)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        updateUi()
+    }
+
+    private fun setupUi() {
+        binding.requestPermission.setOnClickListener {
+            /**
+             * On Android R+, we don't need to request [WRITE_EXTERNAL_STORAGE] to edit 3rd party
+             * media files but we do need to request [READ_EXTERNAL_STORAGE] and explicit user
+             * consent for the edited files.
+             * On Android Q and lower, we need to request both [READ_EXTERNAL_STORAGE] and
+             * [WRITE_EXTERNAL_STORAGE] to edit 3rd party files
+             */
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                requestPermission.launch(arrayOf(READ_EXTERNAL_STORAGE))
+                binding.editImageAttribute.text = "Click to edit last item (write request)"
+            } else {
+                requestPermission.launch(arrayOf(READ_EXTERNAL_STORAGE, WRITE_EXTERNAL_STORAGE))
+                binding.editImageAttribute.text = "Click to edit last item without (write request)"
+            }
+        }
+
+        binding.getMostRecentImage.setOnClickListener {
+            mostRecentImage = StorageUtils.getMostRecentImage(this)
+            binding.editImageAttribute.isEnabled = true
+            binding.imageDetails.text = mostRecentImage.toString()
+        }
+
+        binding.editImageAttribute.setOnClickListener {
+            /**
+             * On Android R+, we use [MediaStore.createWriteRequest] to request
+             * user's consent to be able to edit the file, on Android Q and lower,
+             * we don't have to request that consent to edit if we have the
+             * [READ_EXTERNAL_STORAGE] permission
+             */
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                intentSenderLauncher.launch(
+                    StorageUtils.editImageWithWriteRequest(this, mostRecentImage!!.uri)
+                )
+            } else {
+                StorageUtils.editLastModificationDate(this, mostRecentImage!!.path)
+                mostRecentImage = StorageUtils.getMostRecentImage(this)
+                updateUi()
+            }
+        }
+    }
+
+    private fun updateUi() {
+        val hasPermission = StorageUtils.hasStoragePermission(this)
+        binding.requestPermission.isEnabled = !hasPermission
+        binding.getMostRecentImage.isEnabled = hasPermission
+
+        /**
+         * Display image metadata once it has been fetched
+         */
+        if(mostRecentImage == null) {
+            binding.editImageAttribute.isEnabled = false
+        } else {
+            binding.editImageAttribute.isEnabled = true
+            binding.imageDetails.text = mostRecentImage.toString()
+        }
+    }
+}
